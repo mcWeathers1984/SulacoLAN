f9592b0b96a9dbc8ff77825b3a6821ee74f2155a
mcWeathers1984 <161874523+mcWeathers1984@users.noreply.github.com>
2025-11-17 22:10:38 -0700

    Update main.cpp
    Add main menu

--- netprog/machine-that-goes-ping/src/main.cpp
+++ netprog/machine-that-goes-ping/src/main.cpp
@@ -1,6 +1,18 @@
+#include "common.hpp"
+#include "sstring.hpp"
 
-#include "mcw.hpp"
-int main() {
-    std::cout << "The Machine That Goes PING! (prototype)\n";
-    return 0;
+void MainMenu() {
+ std::println("Main Menu");
+ std::println("(1)\tset ip address");
+ std::println("(2)\tping");
+ std::println("(0)\texit");
+}
+
+void Usage() {
+ std::println("Usage: Ping an ip address, run trace route or set an ip address.\nPress \'0\' to go back or exit.");
+}
+
+int main(int argc, char** argc) {
+ std::print("The Machine That Goes PING!\n");
+ return 0;
 }
